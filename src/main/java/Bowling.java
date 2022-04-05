48f9c8bc08b91b1a83c71494b447e40119a84830
jayshree430 <iyer430@gmail.com>
2022-04-05 22:40:25 +0100

    Set up the first Test and added code in Bowling.java to pass the test

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -1,5 +1,13 @@
 
 public class Bowling {
 
+    private int score = 0;
+    public void roll(int...pins){
+        for (int pin: pins)
+            score +=pin;
+    }
+    public int score(){
+        return score;
+    }
 }
 
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.BeforeEach;
 public class BowlingTest {
     Bowling game;
 
+    @BeforeEach
+    public void setup(){
+        game=new Bowling();
+    }
+    @Test
+    public void checkAllRollsAreZero(){
+        game.roll(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        Assertions.assertEquals(0, game.score());
+    }
 
 
 }
